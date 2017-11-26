04fe82e75fcc3ad0f423b67c15aaffc415fa10f6
imtndA <tsunoda0330@gmail.com>
2017-11-26 13:42:16 +0900

    add fizzbuzz

---
+++ 1/FizzBuzz.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// 引数の数を確認
+	if len(os.Args) != 2 {
+		fmt.Println("引数で一つだけ数字を入力してください。")
+		fmt.Println(os.Args)
+		os.Exit(1)
+	}
+	// 引数で指定された数値を文字列から数値に変換
+	max, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("引数は整数で指定してください")
+	}
+	for i := 1; i < max+1; i++ {
+		// 3か5で割り切れるとき
+		if i%3 == 0 || i%5 == 0 {
+			fmt.Println("FizzBuzz")
+			// 3で割り切れるとき
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+			// 5で割り切れるとき
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+			// 上記以外のとき
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# wacate17winter_repo
+
+WACATE 2017 winter 演習用問題
+このプログラムは意図的にバグを埋め込んでいます。
+
+
